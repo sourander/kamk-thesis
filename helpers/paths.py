e4a4693915463523be7ea361c9e51f84f86a4e03
Jani Sourander <sourander@gmail.com>
2021-08-04 10:42:23 +0300

    Implemented PathMerger and looping.

--- helpers/paths.py
+++ helpers/paths.py
@@ -13,6 +13,7 @@ class PathMerger:
     # Paths. These will be populated during class init.
     staging = None
     bronze = None
+    hive = None
 
     def __init__(self, db, table):
         # Fetch input
@@ -23,5 +24,11 @@ class PathMerger:
         self.generate_paths()
 
     def generate_paths(self):
+        # The simulated "S3 paths"
         self.staging = os.path.join(self.container, self.src_prefix, self.tool, self.ss, self.db, self.table)
         self.bronze = os.path.join(self.container, self.brz_prefix, self.ss, self.db, self.table)
+
+        # Hive
+        table_nous = self.table.replace("_", "")
+        self.hive = f"{self.brz_prefix}.{self.db}_{table_nous}"
+
